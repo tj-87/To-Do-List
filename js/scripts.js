32896da0bffcb8788b6949df124e4b672f137006
Thiago Silva <thiago380tt@gmail.com>
2022-08-30 21:38:02 -0300

    finalizando acao de adicionar o todo

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,47 @@ const filterBtn = document.querySelector("#filter-select");
 
 //Funções
 
+const saveTodo =(text) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTilte = document.createElement("h3");
+    todoTilte.innerText = text;
+    todo.appendChild(todoTilte);
+    
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML =  '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML =  '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleBtn = document.createElement("button");
+    deleBtn.classList.add("remove-todo");
+    deleBtn.innerHTML =  '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleBtn);
+    
+    todolist.appendChild(todo)
+
+    todoInput.value ="";
+    todoInput.focus();
+
+
+
+
+};
+
+// Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const inputValue = todoInput.value;
 
     if (inputValue) {
-        console.log(inputValue);
-        // save todo
+        saveTodo(inputValue)
     }
 
 });
